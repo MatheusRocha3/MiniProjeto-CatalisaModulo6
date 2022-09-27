0be12a90ef046d825ac79c1d719b0598e2c38845
MaxSouza88 <souza1988@hotmail.com>
2022-09-27 18:41:49 -0300

    Atualizando Controller

--- src/main/java/com/miniprojeto/miniprojeto/Controller/EmbalagemController.java
+++ src/main/java/com/miniprojeto/miniprojeto/Controller/EmbalagemController.java
@@ -1,5 +1,27 @@
 package com.miniprojeto.miniprojeto.Controller;
 
+import com.miniprojeto.miniprojeto.Model.EmbalagemModel;
+import com.miniprojeto.miniprojeto.Service.EmbalagemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@RestController
+@RequestMapping(path = "/embalagem")
 public class EmbalagemController {
-    //Para não ficar vazio.
+
+    @Autowired
+    private EmbalagemService embalagemService;
+
+    @GetMapping(path = "/{id}")
+    public Optional<EmbalagemModel> buscaId(@PathVariable Long id){
+        return embalagemService.buscaIdEmbalagem(id);
+    }
+
+
+
 }
