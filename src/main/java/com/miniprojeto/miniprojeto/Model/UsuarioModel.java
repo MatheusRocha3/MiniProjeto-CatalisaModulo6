7ddfd53321818a63cce49ed9c13f20e5320b3739
MaxSouza88 <souza1988@hotmail.com>
2022-09-28 17:27:32 -0300

    Atualizando trabalho

--- src/main/java/com/miniprojeto/miniprojeto/Model/UsuarioModel.java
+++ src/main/java/com/miniprojeto/miniprojeto/Model/UsuarioModel.java
@@ -1,13 +1,14 @@
 package com.miniprojeto.miniprojeto.Model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.miniprojeto.miniprojeto.dto.UsuarioDto;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.time.LocalDate;
 import java.util.List;
 
 
