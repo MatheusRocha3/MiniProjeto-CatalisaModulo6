049f703575eaa3462a614fdf84bf9a05d8be2a5d
MatheusRocha3 <matheuusrocha012@gmail.com>
2022-09-28 19:11:50 -0300

    corrigindo erros

--- src/main/java/com/miniprojeto/miniprojeto/Model/UsuarioModel.java
+++ src/main/java/com/miniprojeto/miniprojeto/Model/UsuarioModel.java
@@ -21,40 +21,29 @@ import java.util.List;
 public class UsuarioModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Basic(optional = false)
     @Column(name = "id", nullable = false)
     private Long id;
-    @Basic(optional = false)
-    @NotNull
-    @Size(min = 1, max = 50)
+
 
 
     @Column(name = "nome_usuario", length = 30)
     private String nomeUsuario;
-    @Basic(optional = false)
-    @NotNull
+
 
     @Column( name = "email_usuario", length = 30)
     private String email;
-    @Basic(optional = false)
-    @NotNull
+
 
     @Column( name=  "cpf_usuario", length = 11)
     private String cpf;
-    @Basic(optional = false)
-    @NotNull
+
 
     @Column(name = "pontos")
     private int pontos;
-    @Basic(optional = false)
-    @NotNull
 
     @JsonIgnore
     @OneToMany(mappedBy = "usuario", cascade = CascadeType.ALL)
     private List<EmbalagemModel> embalagens;
-
-
-
 
     public UsuarioModel(String nomeUsuario, String email, String cpf) {
         this.nomeUsuario = nomeUsuario;
@@ -62,7 +51,6 @@ public class UsuarioModel {
         this.cpf = cpf;
 
     }
-
     public UsuarioModel(Long id, String nomeUsuario, String email, String cpf) {
 
     }
